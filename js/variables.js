de6d964f603f22554a18ad9328c2778a73985ac7
Gonzalo Arroyo <gonzaloarroyo1013@gmail.com>
2021-11-18 20:23:23 -0300

    Entrega de proyecto final, viaje a NY

--- js/variables.js
+++ js/variables.js
@@ -1,23 +1,139 @@
-/*Este archivo contiene las variabnles generales
-para el desarrollo de MOTORES FERCHU*/
+/* menu */
 
-let products = [
-    {id: 1, product: "motor", mark: 'Honda', price: 300, stock:2, model: 'fk1', img: "https://http2.mlstatic.com/D_NQ_NP_746920-MLA47136187723_082021-O.webp"},
-    {id: 2, product: "motor", mark: 'Yamaha', price: 250, stock:1, model: 'ss5' ,img: "https://http2.mlstatic.com/D_NQ_NP_819917-MLA25535399336_042017-O.webp"},
-    {id: 3, product: "motor", mark: 'Mercury', price: 310, stock:2, model: '015', img: "https://http2.mlstatic.com/D_NQ_NP_885559-MLA46366391624_062021-O.webp"},
-    {id: 4, product: "motor", mark: 'Suzuky', price: 210, stock:1, model: 'SUKI', img: "https://http2.mlstatic.com/D_NQ_NP_757609-MLA47949945193_102021-O.webp"},
-    {id: 5, product: "motor", mark: 'PARSUN 15hp', price: 200, stock:1, model: 'Largo', img: "https://http2.mlstatic.com/D_NQ_NP_973846-MLA46543438010_062021-O.webp"},
-    {id: 6, product: "motor", mark: 'Mercury 25hp', price: 150, stock:3, model: 'sea pro', img: "https://http2.mlstatic.com/D_NQ_NP_755147-MLA47858619700_102021-O.webp"},
-    {id: 7, product: "chaleco", mark: 'Daf Nautic', price: 30, stock:2, model: 'deportivo', img: "https://http2.mlstatic.com/D_NQ_NP_886189-MLA43459267440_092020-O.webp"},
-    {id: 8, product: "chaleco", mark: 'Oxigeno', price: 25, stock:1, model: 'poncho', img: "https://http2.mlstatic.com/D_NQ_NP_172101-MLA20276425318_042015-O.webp"},
-    {id: 9, product: "chaleco", mark: 'EMP Nautica', price: 40, stock:2, model: 'KayakLife cordura rojo', img: "https://http2.mlstatic.com/D_NQ_NP_697899-MLA31981395827_082019-O.webp"},
-    {id: 10, product: "sombrilla", mark: 'Albatros', price: 10, stock:1, model: '170T', img: "https://http2.mlstatic.com/D_NQ_NP_841187-MLA47340011304_092021-O.webp"},
-    {id: 11, product: "sombrilla", mark: '	Rc Rough Challenge', price: 15, stock:1, model: 'Sombrilla 2.40', img: "https://http2.mlstatic.com/D_NQ_NP_672074-MLA31119171859_062019-O.webp"},
-    {id: 12, product: "Conservadora", mark: 'Garden Life', price: 15, stock:3, model: 'LF7732', img: "https://http2.mlstatic.com/D_NQ_NP_751465-MLA45062946982_032021-O.webp"},
-    {id: 13, product: "Conservadora", mark: 'Colombraro', price: 25, stock:3, model: '4246', img: "https://http2.mlstatic.com/D_NQ_NP_767379-MLA33083275601_122019-O.webp"},
-    {id: 14, product: "Conservadora", mark: 'Colombraro', price: 15, stock:3, model: '4232', img: "https://http2.mlstatic.com/D_NQ_NP_743870-MLA44274108266_122020-O.webp"},
-];
+const hamburguer = document.querySelector('.hamburguer')
+const menu = document.querySelector('.menu-navegacion')
 
-const selectedQty = 'Indicanos la cantidad que deseas';
+console.log(menu)
+console.log(hamburguer)
 
-let cart = 0;
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread')
+    && e.target != menu && e.target != hamburguer){
+        menu.classList.toggle("spread")
+    }
+})
+
+/* carrito */
+
+const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
+addToShoppingCartButtons.forEach((addToCartButton) => {
+  addToCartButton.addEventListener('click', addToCartClicked);
+});
+
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClicked);
+
+const shoppingCartItemsContainer = document.querySelector(
+  '.shoppingCartItemsContainer'
+);
+
+function addToCartClicked(event) {
+  const button = event.target;
+  const item = button.closest('.item');
+
+  const itemTitle = item.querySelector('.item-title').textContent;
+  const itemPrice = item.querySelector('.item-price').textContent;
+  const itemImage = item.querySelector('.item-image').src;
+
+  addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+}
+
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+  const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
+    'shoppingCartItemTitle'
+  );
+  for (let i = 0; i < elementsTitle.length; i++) {
+    if (elementsTitle[i].innerText === itemTitle) {
+      let elementQuantity = elementsTitle[
+        i
+      ].parentElement.parentElement.parentElement.querySelector(
+        '.shoppingCartItemQuantity'
+      );
+      elementQuantity.value++;
+      $('.toast').toast('show');
+      updateShoppingCartTotal();
+      return;
+    }
+  }
+
+  const shoppingCartRow = document.createElement('div');
+  const shoppingCartContent = `
+  <div class="row shoppingCartItem">
+        <div class="col-6">
+            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <img src=${itemImage} class="shopping-cart-image">
+                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <div
+                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                    value="1">
+                <button class="btn btn-danger buttonDelete" type="button">X</button>
+            </div>
+        </div>
+    </div>`;
+  shoppingCartRow.innerHTML = shoppingCartContent;
+  shoppingCartItemsContainer.append(shoppingCartRow);
+
+  shoppingCartRow
+    .querySelector('.buttonDelete')
+    .addEventListener('click', removeShoppingCartItem);
+
+  shoppingCartRow
+    .querySelector('.shoppingCartItemQuantity')
+    .addEventListener('change', quantityChanged);
+
+  updateShoppingCartTotal();
+}
+
+function updateShoppingCartTotal() {
+  let total = 0;
+  const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+
+  const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+
+  shoppingCartItems.forEach((shoppingCartItem) => {
+    const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
+      '.shoppingCartItemPrice'
+    );
+    const shoppingCartItemPrice = Number(
+      shoppingCartItemPriceElement.textContent.replace('u$d', '')
+    );
+    const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
+      '.shoppingCartItemQuantity'
+    );
+    const shoppingCartItemQuantity = Number(
+      shoppingCartItemQuantityElement.value
+    );
+    total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+  });
+  shoppingCartTotal.innerHTML = `${total.toFixed(2)}u$d`;
+}
+
+function removeShoppingCartItem(event) {
+  const buttonClicked = event.target;
+  buttonClicked.closest('.shoppingCartItem').remove();
+  updateShoppingCartTotal();
+}
+
+function quantityChanged(event) {
+  const input = event.target;
+  input.value <= 0 ? (input.value = 1) : null;
+  updateShoppingCartTotal();
+}
+
+function comprarButtonClicked() {
+  shoppingCartItemsContainer.innerHTML = '';
+  updateShoppingCartTotal();
+}
